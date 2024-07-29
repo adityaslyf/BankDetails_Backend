fa30cdeb38b984f6ff16e5f22aef30a3fd10b3b0
adityaslyf <aditya.varshneymail@gmail.com>
2024-07-30 02:22:29 +0530

    connected to db

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,10 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-});
-
-pool.on('connect', () => {
-  console.log('Connected to the database');
 });
 
 export default pool;
